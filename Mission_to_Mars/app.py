939557db010ba04fee202c164d1a5ba6bccef0ab
Rahul Dahya <58918510+rahuldahya11@users.noreply.github.com>
2020-05-27 22:26:32 -0500

    commit

---
+++ Mission_to_Mars/app.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, render_template, request
+from flaskpymongo import PyMongo
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+    return "Welcome to my 'Home' page!"
+
+@app.route("/about")
+def about():
+    print("Server received request for 'About' page...")
+    return "Welcome to my 'About' page!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
